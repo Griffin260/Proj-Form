0b753e5ba24955516fc868d397c752772f5b8259
Moisés <88942082+Moises-dspereira@users.noreply.github.com>
2022-04-04 13:04:42 -0300

    o javascript esta validando mas mesmo assim, continua saindo do formulario

--- functions.js
+++ functions.js
@@ -1,53 +1,47 @@
-function validar() {
+
+function validar(){
     var nome = formuser.nome.value;
     var email = formuser.email.value;
     var fone = formuser.fone.value;
     var valor = formuser.valor.value;
     var parcela = formuser.parcela.value;
 
-    if (nome == "") {
+    if(nome == ""){
         alert('preencha o campo Nome!');
         formeuser.nome.focus();
         return false;
     }
 
-    if (email == "" || email.indexOf('@') == -1) {
+    if(email == ""  || email.indexOf('@') == -1 ){
         alert('preencha o campo E_mail!');
         formeuser.email.focus();
-        return false;
+        return false;  
     }
 
-    if (fone == "") {
+    if(fone == ""){
         alert('preencha o campo Telefone!');
         formeuser.fone.focus();
-        return false;
+        return false;  
     }
 
-    if (valor == "") {
+    if(valor == ""){
         alert('Preencha o campo Valor Desejado!');
         formeuser.valor.focus();
-        return false;
-
-        if (valor < 1000) {
+        return false;  
+         }else if( valor < 1000){
             alert('O valor minimo R$1000!');
-        }
-
-        if (valor > 10000) {
-            alert('O valor maximo  R$10000!');
-        }
-    }
-
-    if (parcela == "") {
+                }else if ( valor > 10000){
+                     alert('O valor maximo  R$10000!');
+                }
+          
+    if(parcela == ""){
         alert('Preencha o campo Valor da parcela!');
         formeuser.valor.focus();
-        return false;
-
-        if (parcela < 2) {
+        return false;  
+         }else if( parcela < 2){
             alert('Numero de minimo parcelas 2 vezes!');
-        }
-
-        if (parcela > 24) {
-            alert('Numero de maximo de parcelas 24vezes!');
-        }
-    }
+                }else if ( parcela > 24){
+                     alert('Numero de maximo de parcelas 24vezes!');
+                }         
 }
+
